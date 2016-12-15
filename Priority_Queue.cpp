62e76a593c77b79cc42165fa856bb3c54e283c62
floresu <uriel_f10@yahoo.com>
2016-12-15 18:59:18 -0500

    Add files via upload

---
+++ Priority_Queue.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main()
+{
+	priority_queue<int> queue;
+	
+	// Values that are inserted.
+	queue.push(5);
+	queue.push(39);
+	queue.push(20);
+	queue.push(55);
+	queue.push(88);
+	queue.push(100);
+	queue.push(23);
+	queue.push(19);
+
+	
+	// Loop 8 times.
+	for (int i = 0; i < 8; i++)
+	{
+		cout << queue.top() << endl;		// Prints the value of the queue.
+		queue.pop(); 						// Pop value from the top of the queue.
+	}
+
+	return 0;
+}
